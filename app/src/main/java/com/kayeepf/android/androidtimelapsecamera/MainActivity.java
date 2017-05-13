5d7d14df5893d147cb41cfeef457639afaca8b1a
kayeepf <kayeepf@hotmail.com>
2017-05-13 23:27:36 +0800

    add an empty MainActivity

---
+++ app/src/main/java/com/kayeepf/android/androidtimelapsecamera/MainActivity.java
@@ -0,0 +1,29 @@
+package com.kayeepf.android.androidtimelapsecamera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by huangshifeng on 2017/5/13.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
