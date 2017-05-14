1591c02a43d9229861f5e199f88a8ce464b08b7d
kayeepf <kayeepf@hotmail.com>
2017-05-14 09:12:47 +0800

    change order for reading

--- app/src/main/java/com/kayeepf/android/androidtimelapsecamera/MainActivity.java
+++ app/src/main/java/com/kayeepf/android/androidtimelapsecamera/MainActivity.java
@@ -13,8 +13,8 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    protected void onResume() {
+        super.onResume();
     }
 
     @Override
@@ -23,7 +23,7 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
