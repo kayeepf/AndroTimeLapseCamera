a3e3a42b9abe8f32e037286a156800aa143f1f1b
kayeepf <kayeepf@hotmail.com>
2017-05-14 10:47:52 +0800

    implements camera preview

--- app/src/main/java/com/kayeepf/android/androidtimelapsecamera/SimpleCameraActivity.java
+++ app/src/main/java/com/kayeepf/android/androidtimelapsecamera/SimpleCameraActivity.java
@@ -2,11 +2,14 @@ package com.kayeepf.android.androidtimelapsecamera;
 
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.RelativeLayout;
+
+import java.io.IOException;
 
 /**
  * Created by huangshifeng on 2017/5/14.
@@ -18,7 +21,7 @@ public class SimpleCameraActivity extends Activity implements SurfaceHolder.Call
     SurfaceView surfaceView;
     SurfaceHolder surfaceHolder;
 
-    static boolean checkHardwareOk = false;
+    Camera camera = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +44,8 @@ public class SimpleCameraActivity extends Activity implements SurfaceHolder.Call
         super.onResume();
         if(!getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
             Log.w(log_tag,"No Camera Feature");
-            checkHardwareOk = false;
             this.finish();
         }
-        checkHardwareOk = true;
         Log.i(log_tag, "<<<onResume()");
     }
 
@@ -52,14 +53,6 @@ public class SimpleCameraActivity extends Activity implements SurfaceHolder.Call
     protected void onPause() {
         Log.i(log_tag,">>>onPause()");
         super.onPause();
-        if(!checkHardwareOk)
-        {
-
-        }
-        else
-        {
-
-        }
         Log.i(log_tag, "<<<onPause()");
     }
 
@@ -73,18 +66,39 @@ public class SimpleCameraActivity extends Activity implements SurfaceHolder.Call
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         Log.i(log_tag,">>>surfaceCreated()");
+        if(getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            camera = camera.open();
+            Camera.Parameters cameraParam = camera.getParameters();
+            camera.setParameters(cameraParam);
+            camera.setDisplayOrientation(90);
+        }
         Log.i(log_tag,"<<<surfaceCreated()");
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
         Log.i(log_tag,">>>surfaceChanged()");
+        if(getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA) &&
+                camera != null) {
+            try {
+                camera.setPreviewDisplay(surfaceHolder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            camera.startPreview();
+        }
         Log.i(log_tag,"<<<surfaceChanged()");
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
         Log.i(log_tag,">>>surfaceDestroyed()");
+        if(getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA) &&
+                camera != null) {
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
         Log.i(log_tag,"<<<surfaceDestroyed()");
     }
 }
